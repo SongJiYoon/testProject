4a96dc3e30729f393a84618aaa2676013b7cfe91
kyowon <kyowon@10.6.8.31>
2021-07-05 17:42:00 +0900

    test

---
+++ testProject/projectService/src/main/java/com/test/project/service/common/CommonService.java
@@ -0,0 +1,10 @@
+package com.test.project.service.common;
+
+public class CommonService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("됌?");
+	}
+
+}
