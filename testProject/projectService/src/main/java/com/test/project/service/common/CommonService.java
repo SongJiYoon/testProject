300d85c96bec24d5abead3b60406dcfa3a0e154d
kyowon <kyowon@10.6.8.31>
2021-07-06 18:02:26 +0900

    test

--- testProject/projectService/src/main/java/com/test/project/service/common/CommonService.java
+++ testProject/projectService/src/main/java/com/test/project/service/common/CommonService.java
@@ -1,10 +1,23 @@
 package com.test.project.service.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
 public class CommonService {
 
+	@Value("${test.name}")
+	static String test;
+	
+	static Logger logger = LoggerFactory.getLogger(CommonService.class);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("됌?");
+			logger.info("돼네?");
+			
+			System.out.println(test);
+			
 	}
 
 }
